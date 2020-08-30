b127ec3b27e0289b6887d42f0c274157c18789dd
Trong Hieu <hieuntctk42@gmail.com>
2020-08-30 15:09:09 +0700

    init

---
+++ 01.pokedex/src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Pokecard from './Pokecard';
+
+class App extends React.Component {
+  render() {
+    return <Pokecard id={4} name="Charmander" type="fire" exp={62} />;
+  }
+}
+
+export default App;
